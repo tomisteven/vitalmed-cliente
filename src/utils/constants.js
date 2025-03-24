6d8f2f9b911d4bee84f41531463483a8a91dae54
Tomas Steven Desktop <tomssteven22@gmail.com>
2025-03-24 13:55:03 -0300

    login

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-//const url = "https://exuberant-joelle-digitalcode-937bee37.koyeb.app";
-const url = "http://localhost:8082";
+const url = "https://exuberant-joelle-digitalcode-937bee37.koyeb.app";
+//const url = "http://localhost:8082";
 
 const production = true;
 
